8f02bcd53cc1db9436dd9b842772c875cb6106ff
STEVANUS BILLY <stevanusbilly00@mail.com>
2020-11-26 16:35:32 +0700

    ganbar dummy

--- database/seeds/ImageTableSeeder.php
+++ database/seeds/ImageTableSeeder.php
@@ -14,185 +14,184 @@ class ImageTableSeeder extends Seeder
     {
         DB::table('image')->insert(array(
             'game_id' => '1',
-            'link' => 'https://cdn2.unrealengine.com/egs-outriders-peoplecanfly-g1a-02-1920x1080-285554257.jpg?h=1080&resize=1&w=1920'
+            'link' => '1_1'
         ));
         DB::table('image')->insert(array(
             'game_id' => '1',
-            'link' => 'https://cdn2.unrealengine.com/egs-outriders-peoplecanfly-g1a-03-1920x1080-285553599.jpg?h=1080&resize=1&w=1920'
+            'link' => '1_2'
         ));
         DB::table('image')->insert(array(
             'game_id' => '1',
-            'link' => 'https://cdn2.unrealengine.com/egs-outriders-peoplecanfly-g1a-04-1920x1080-285553574.jpg?h=1080&resize=1&w=1920'
+            'link' => '1_logo'
         ));
         DB::table('image')->insert(array(
             'game_id' => '2',
-            'link' => 'https://cdn2.unrealengine.com/acv-100-norway-3840x2160-277660461.jpg'
+            'link' => '2_1'
         ));
         DB::table('image')->insert(array(
             'game_id' => '2',
-            'link' => 'https://cdn2.unrealengine.com/acv-101-urbanstealth-emea-3840x2160-277668968.jpg'
+            'link' => '2_2'
         ));
         DB::table('image')->insert(array(
             'game_id' => '2',
-            'link' => 'https://cdn2.unrealengine.com/acv-102-fenririncave-3840x2160-277663542.jpg'
+            'link' => '2_logo'
         ));
         DB::table('image')->insert(array(
             'game_id' => '3',
-            'link' => 'https://cdn2.unrealengine.com/acv-100-norway-3840x2160-277660461.jpg'
+            'link' => '3_1'
         ));
         DB::table('image')->insert(array(
             'game_id' => '3',
-            'link' => 'https://cdn2.unrealengine.com/acv-101-urbanstealth-emea-3840x2160-277668968.jpg'
+            'link' => '3_2'
         ));
         DB::table('image')->insert(array(
             'game_id' => '3',
-            'link' => 'https://cdn2.unrealengine.com/acv-102-fenririncave-3840x2160-277663542.jpg'
+            'link' => '3_logo'
         ));
         DB::table('image')->insert(array(
             'game_id' => '4',
-            'link' => 'https://cdn2.unrealengine.com/Diesel%2Fproduct%2Flarkspur%2FFCP_Pet_Wolf_GOLD_1080p-1920x1080-303560c9f4493db5b1926d4bdf026432e12edc4b.jpg'
+            'link' => '4_1'
         ));
         DB::table('image')->insert(array(
             'game_id' => '4',
-            'link' => 'https://cdn2.unrealengine.com/Diesel%2Fproduct%2Flarkspur%2FFarCryPrimal_Screenshots_Izila-1920x1080-3ce9527c9216f147fa904ff212c2e587917cc532.jpg'
+            'link' => '4_2'
         ));
         DB::table('image')->insert(array(
             'game_id' => '4',
-            'link' => 'https://cdn2.unrealengine.com/Diesel%2Fproduct%2Flarkspur%2FFCP_Udam_GOLD_1080p-1920x1080-a97b6d724fe544e763c65dc0589d2ef9f03f6a28.jpg'
+            'link' => '4_logo'
         ));
         DB::table('image')->insert(array(
             'game_id' => '5',
-            'link' => 'https://cdn2.unrealengine.com/Diesel%2Fproduct%2Flarkspur%2FFCP_Pet_Wolf_GOLD_1080p-1920x1080-303560c9f4493db5b1926d4bdf026432e12edc4b.jpg'
+            'link' => '5_1'
         ));
         DB::table('image')->insert(array(
             'game_id' => '5',
-            'link' => 'https://cdn2.unrealengine.com/Diesel%2Fproduct%2Flarkspur%2FFarCryPrimal_Screenshots_Izila-1920x1080-3ce9527c9216f147fa904ff212c2e587917cc532.jpg'
+            'link' => '5_2'
         ));
         DB::table('image')->insert(array(
             'game_id' => '5',
-            'link' => 'https://cdn2.unrealengine.com/Diesel%2Fproduct%2Flarkspur%2FFCP_Udam_GOLD_1080p-1920x1080-a97b6d724fe544e763c65dc0589d2ef9f03f6a28.jpg'
+            'link' => '5_logo'
         ));
         DB::table('image')->insert(array(
             'game_id' => '6',
-            'link' => 'https://cdn2.unrealengine.com/egs-footballmanager2021-sportsinteractive-s1-2560x1440-070457508.jpg?h=1080&resize=1&w=1920'
+            'link' => '6_1'
         ));
         DB::table('image')->insert(array(
             'game_id' => '6',
-            'link' => 'https://cdn2.unrealengine.com/egs-footballmanager2021-sportsinteractive-g1a-00-1920x1080-442650083.jpg?h=1080&resize=1&w=1920'
+            'link' => '6_2'
         ));
         DB::table('image')->insert(array(
             'game_id' => '6',
-            'link' => 'https://cdn2.unrealengine.com/egs-footballmanager2021-sportsinteractive-g1a-01-1920x1080-442649702.jpg?h=1080&resize=1&w=1920'
+            'link' => '6_logo'
         ));
         DB::table('image')->insert(array(
             'game_id' => '7',
-            'link' => 'https://cdn2.unrealengine.com/egs-thejackboxpartypack7-jackboxgamesinc-g1a-02-1920x1080-225380876.jpg?h=1080&resize=1&w=1920'
+            'link' => '7_1'
         ));
         DB::table('image')->insert(array(
             'game_id' => '7',
-            'link' => 'https://cdn2.unrealengine.com/egs-thejackboxpartypack7-jackboxgamesinc-g1a-03-1920x1080-225380905.jpg?h=1080&resize=1&w=1920'
+            'link' => '7_2'
         ));
         DB::table('image')->insert(array(
             'game_id' => '7',
-            'link' => 'https://cdn2.unrealengine.com/egs-thejackboxpartypack7-jackboxgamesinc-g1a-04-1920x1080-225380984.jpg?h=1080&resize=1&w=1920'
+            'link' => '7_logo'
         ));
         DB::table('image')->insert(array(
             'game_id' => '8',
-            'link' => 'https://cdn2.unrealengine.com/Diesel%2Fproductv2%2Fsnowrunner%2Fhome%2FSRACCOLADESSCORESPRESS-3840x2160-cbc8668874c154b79445b22755b3080089f1789a.png?h=1080&resize=1&w=1920'
+            'link' => '8_1'
         ));
         DB::table('image')->insert(array(
             'game_id' => '8',
-            'link' => 'https://cdn2.unrealengine.com/Diesel%2Fproductv2%2Fsnowrunner%2Fhome%2FSnowRunner_Screenshot_03_nologo-1920x1080-4b2a0b922c126a679339b433fa89f817f7fedb39.jpg'
+            'link' => '8_2'
         ));
         DB::table('image')->insert(array(
             'game_id' => '8',
-            'link' => 'https://cdn2.unrealengine.com/Diesel%2Fproductv2%2Fsnowrunner%2Fhome%2FSnowRunner_Screenshot_01_nologo-1920x1080-53d86bf87615b553f7fc071e1fe42aa7837b59af.jpg'
+            'link' => '8_logo'
         ));
         DB::table('image')->insert(array(
             'game_id' => '9',
-            'link' => 'https://cdn2.unrealengine.com/Diesel%2Fproductv2%2Fsnowrunner%2Fhome%2FSRACCOLADESSCORESPRESS-3840x2160-cbc8668874c154b79445b22755b3080089f1789a.png?h=1080&resize=1&w=1920'
+            'link' => '9_1'
         ));
         DB::table('image')->insert(array(
             'game_id' => '9',
-            'link' => 'https://cdn2.unrealengine.com/Diesel%2Fproductv2%2Fsnowrunner%2Fhome%2FSnowRunner_Screenshot_03_nologo-1920x1080-4b2a0b922c126a679339b433fa89f817f7fedb39.jpg'
+            'link' => '9_2'
         ));
         DB::table('image')->insert(array(
             'game_id' => '9',
-            'link' => 'https://cdn2.unrealengine.com/Diesel%2Fproductv2%2Fsnowrunner%2Fhome%2FSnowRunner_Screenshot_01_nologo-1920x1080-53d86bf87615b553f7fc071e1fe42aa7837b59af.jpg'
+            'link' => '9_logo'
         ));
         DB::table('image')->insert(array(
             'game_id' => '10',
-            'link' => 'https://cdn2.unrealengine.com/Diesel%2Fproductv2%2Ftotal-war-saga-troy%2Fhome%2FEGS_TotalWarSagaTROY_CREATIVEASSEMBLY_G2_00-1920x1080-f84797effd32be0dbcb1749a5fb1b720d7c8b880.jpg'
+            'link' => '10_1'
         ));
         DB::table('image')->insert(array(
             'game_id' => '10',
-            'link' => 'https://cdn2.unrealengine.com/Diesel%2Fproductv2%2Ftotal-war-saga-troy%2Fhome%2FEGS_TotalWarSagaTROY_CREATIVEASSEMBLY_G2_01-1920x1080-523f2682b9730edd12c3b5124a61f44632a736fd.jpg'
+            'link' => '10_2'
         ));
         DB::table('image')->insert(array(
             'game_id' => '10',
-            'link' => 'https://cdn2.unrealengine.com/Diesel%2Fproductv2%2Ftotal-war-saga-troy%2Fhome%2FEGS_TotalWarSagaTROY_CREATIVEASSEMBLY_G2_02-1920x1080-3dfbd6dd7bcdb7e4ce1af9e92eb9ea649e0c8cbf.jpg'
+            'link' => '10_logo'
         ));
         DB::table('image')->insert(array(
             'game_id' => '11',
-            'link' => 'https://cdn2.unrealengine.com/Diesel%2Fproductv2%2Fdisco-elysium%2Fhome%2FEGS_DiscoElysium_ZAUM_G1A_00-1920x1080-41330fe9b0795aa67169701fe9dc0c74005c8853.jpg?h=1080&resize=1&w=1920'
+            'link' => '11_1'
         ));
         DB::table('image')->insert(array(
             'game_id' => '11',
-            'link' => 'https://cdn2.unrealengine.com/Diesel%2Fproductv2%2Fdisco-elysium%2Fhome%2FEGS_DiscoElysium_ZAUM_G1A_01-1920x1080-60fe24eca41d5daeb10376ac6af3c3a0da17e490.jpg?h=1080&resize=1&w=1920'
+            'link' => '11_2'
         ));
         DB::table('image')->insert(array(
             'game_id' => '11',
-            'link' => 'https://cdn2.unrealengine.com/Diesel%2Fproductv2%2Fdisco-elysium%2Fhome%2FEGS_DiscoElysium_ZAUM_G1A_02-1920x1080-6b67d0f1ad535720d9c2b267e8f32a1a8609adf8.jpg?h=1080&resize=1&w=1920'
+            'link' => '11_logo'
         ));
         DB::table('image')->insert(array(
             'game_id' => '12',
-            'link' => 'https://cdn2.unrealengine.com/featured-carousel-desktop-horizontal-1360x766-173902157.png?h=1080&resize=1&w=1920'
+            'link' => '12_1'
         ));
         DB::table('image')->insert(array(
             'game_id' => '12',
-            'link' => 'https://cdn2.unrealengine.com/boxart-character-3840x2160-589989153.jpg?h=1080&resize=1&w=1920'
+            'link' => '12_2'
         ));
         DB::table('image')->insert(array(
             'game_id' => '12',
-            'link' => 'https://cdn2.unrealengine.com/ih-screenshot-50-boxart-actionshot-4096x2304-926075053.png?h=1080&resize=1&w=1920'
+            'link' => '12_logo'
+        ));
+        DB::table('image')->insert(array(
+            'game_id' => '3',
+            'link' => '13_1'
         ));
         DB::table('image')->insert(array(
             'game_id' => '13',
-            'link' => 'https://cdn2.unrealengine.com/featured-carousel-desktop-horizontal-1360x766-175260777.jpg?h=1080&resize=1&w=1920'
+            'link' => '13_2'
         ));
         DB::table('image')->insert(array(
             'game_id' => '13',
-            'link' => 'https://cdn2.unrealengine.com/boxart-character-3840x2160-589989153.jpg?h=1080&resize=1&w=1920'
-        ));
-        DB::table('image')->insert(array(
-            'game_id' => '13',
-            'link' => 'https://cdn2.unrealengine.com/ih-screenshot-50-boxart-actionshot-4096x2304-926075053.png?h=1080&resize=1&w=1920'
+            'link' => '13_logo'
         ));
         DB::table('image')->insert(array(
             'game_id' => '14',
-            'link' => 'https://cdn2.unrealengine.com/egs-rocketleague-psyonixllc-s5-1920x1080-010136059.jpg?h=1080&resize=1&w=1920'
+            'link' => '14_1'
         ));
         DB::table('image')->insert(array(
             'game_id' => '14',
-            'link' => 'https://cdn2.unrealengine.com/egs-rocketleague-psyonixllc-g1a-01-1920x1080-158045320.jpg?h=1080&resize=1&w=1920'
+            'link' => '14_2'
         ));
         DB::table('image')->insert(array(
             'game_id' => '14',
-            'link' => 'https://cdn2.unrealengine.com/egs-rocketleague-psyonixllc-g1a-02-1920x1080-158045273.jpg?h=1080&resize=1&w=1920'
+            'link' => '14_logo'
         ));
         DB::table('image')->insert(array(
             'game_id' => '15',
-            'link' => 'https://cdn2.unrealengine.com/egs-elitedangerous-frontierdevelopments-g1a-00-1920x1080-432484602.jpg?h=1080&resize=1&w=1920'
+            'link' => '15_1'
         ));
         DB::table('image')->insert(array(
             'game_id' => '15',
-            'link' => 'https://cdn2.unrealengine.com/egs-elitedangerous-frontierdevelopments-g1a-01-1920x1080-432484775.jpg?h=1080&resize=1&w=1920'
+            'link' => '15_2'
         ));
         DB::table('image')->insert(array(
             'game_id' => '15',
-            'link' => 'https://cdn2.unrealengine.com/egs-elitedangerous-frontierdevelopments-g1a-02-1920x1080-432490145.jpg?h=1080&resize=1&w=1920'
+            'link' => '15_logo'
         ));
-
 
     }
 }
