bcde30c776f5a3201320974148fcd2ced03bea7b
STEVANUS BILLY <stevanusbilly00@mail.com>
2020-12-03 16:23:57 +0700

    ubah email tawk to

--- resources/views/tawkto.blade.php
+++ resources/views/tawkto.blade.php
@@ -4,7 +4,7 @@
     (function(){
     var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
     s1.async=true;
-    s1.src='https://embed.tawk.to/5f5ae8814704467e89edfd9f/default';
+    s1.src='https://embed.tawk.to/5fc8ae2da1d54c18d8eff19d/default';
     s1.charset='UTF-8';
     s1.setAttribute('crossorigin','*');
     s0.parentNode.insertBefore(s1,s0);
